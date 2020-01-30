328990ecd430db1f33f2ce1ddf4e35d9b5fd37d8
Benito Galvez <mystwalker131@gmail.com>
2020-01-30 20:32:46 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,20 @@
 #write your code here
 
-def countdown
+def countdown(secs)
+  
+   until secs == 0
+ puts "#{secs} SECOND(S)"
+ number += 1
+secs -=1
+end
+   "HAPPY NEW YEAR!"
+end
+
+def countdown _with_sleep(secs)
+  while secs>0
+  puts "#{secs} SECOND(s)"
+  sleep(1)
+  secs -=1
+end
+"Happy New Year!"
 end
